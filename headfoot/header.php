255b1b0511a20155b82b155cb059eed266c46632
Trebon_Deon <IsaevSlava.2001@yandex.ru>
2021-08-02 17:02:33 +0300

    1.0.0(headers, index)

---
+++ headfoot/header.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../styles/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <!--подкрепить данные о пользователе из сессии!-->
+    <a href="../auth.php" >Авторизация/Регистрация</a>
+    <a href="../lk.php">Личный кабинет</a>
+    <!--обучающийся!-->
+    <a href="../prepods.php">Список преподавателей</a>
+    <a href="../courses.php">Список курсов</a>
+    <a href="../predmets.php">Список предметов</a>
+    <a href="../journal.php">Журнал</a>
+    <a href="../materials.php">Список материалов</a>
+    <!-----------------!-->
+    <!--родитель!-->
+    <a href="../prepods.php">Список преподавателей</a>
+    <a href="../courses.php">Список курсов</a>
+    <a href="../childrens.php">Список детей</a>
+    <a href="../predmets.php">Список предметов</a>
+    <a href="../journal.php">Журнал</a>
+    <a href="../materials.php">Список материалов</a>
+    <!-----------------!-->
+    <!--преподаватель!-->
+    <a href="../pupils.php">Список учеников</a>
+    <a href="../courses.php">Список курсов</a>
+    <a href="../predmets.php">Список предметов</a>
+    <a href="../journal.php">Журнал</a>
+    <a href="../materials.php">Список материалов</a>
+    <!-----------------!-->
+    <!--администратор!-->
+    <a href="../prepods.php">Список преподавателей</a>
+    <a href="../courses.php">Список курсов</a>
+    <a href="../predmets.php">Список предметов</a>
+    <a href="../parents.php">Список родителей</a>
+    <a href="../moders.php">Список модераторов</a>
+    <a href="../students.php">Список обучающихся</a>
+    <a href="../journal.php">Журнал</a>
+    <a href="../materials.php">Список материалов</a>
+    <!-----------------!-->
+    <!--модератор!-->
+    <a href="../prepods.php">Список преподавателей</a>
+    <a href="../courses_p.php">Список курсов</a>
+    <a href="../predmets.php">Список предметов</a>
+    <a href="../journal.php">Журнал</a>
+    <a href="../materials.php">Список материалов</a>
+    <!-----------------!-->
+
+</body>
+</html>
