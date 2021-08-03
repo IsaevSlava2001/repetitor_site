335892413a7cee8b3798660b20e4e005dd6e44e9
Trebon_Deon <IsaevSlava.2001@yandex.ru>
2021-08-03 19:04:26 +0300

    1.0.8 updated auth and DB_plan

--- reg.php
+++ reg.php
@@ -7,10 +7,11 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="reg_check.php" method="post"></form>
+    <form action="reg_check.php" method="post">
     Введите фамилию<input type="text" name="famil" id=""><br>
     Введите имя<input type="text" name="name" id=""><br>
     Введите отчество<input type="text" name="father_name" id=""><br>
+    Введите дату рождения<input type="date" name="date_of_birth" id=""><br>
     Выберите роль
     <select name="role" id="role_id" onchange="output_textarea()">
         <option value="1">Ученик</option>
@@ -21,7 +22,8 @@
     Введите почту<input type="email" name="mail"><br>
     Введите пароль<input type="password" name="password"><br>
     Повторите пароль<input type="password" name="password"><br>
-    <input type="submit" value="Зарегистрироваться">
+    <input type="submit" value="Подтвердить">
+    <input type="reset">
 </form>
 </body>
     <script src="/scripts/script_auth.js"></script>
